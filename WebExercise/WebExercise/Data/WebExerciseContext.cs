b10e2cb829d7ac630f38a84ff44f7a908085b877
LeiMuu <hugo83101@gmail.com>
2019-08-29 16:04:18 +0800

    Asp.Net Core 2.2 MVC教學課程 使用料庫

---
+++ WebExercise/WebExercise/Data/WebExerciseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebExercise.Models
+{
+    public class WebExerciseContext : DbContext
+    {
+        public WebExerciseContext (DbContextOptions<WebExerciseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebExercise.Models.Movie> Movie { get; set; }
+    }
+}
